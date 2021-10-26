91fb745c48e05f3fdc4ac9cced7a71cc386e5806
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-10-26 16:45:41 +0200

    Prepare for 0.02 version

--- V4.pm
+++ V4.pm
@@ -11,7 +11,7 @@ use IO::Barf qw(barf);
 use JSON::XS;
 use LWP::UserAgent;
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Constructor.
 sub new {
@@ -259,6 +259,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
--- t/App-Kramerius-V4/02-version.t
+++ t/App-Kramerius-V4/02-version.t
@@ -6,4 +6,4 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-is($App::Kramerius::V4::VERSION, 0.01, 'Version.');
+is($App::Kramerius::V4::VERSION, 0.02, 'Version.');
